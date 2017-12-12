1962b7f90424f7999dab32d2dd5b6dc7c614c101
Rob Welan <robwelan@yahoo.com.au>
2017-12-12 16:30:27 +1000

    Update readme.md

--- src/predictable-state-container/store/retrieved-data.js
+++ src/predictable-state-container/store/retrieved-data.js
@@ -1,7 +1,31 @@
-const initialState = {}
+import { LOAD_DATA_LIST, LOAD_DATA_RACE, TOGGLE_DATA_DISPLAY } from '../actions/retrieve-data'
+const initialState = {
+	list: [],
+	race: [],
+	detail: false
+}
 
 const data = (state = initialState, action) => {
-	return state
+	switch (action.type) {
+		case LOAD_DATA_LIST:
+			return {
+				...state,
+				list: [...action.list]
+			}
+		case LOAD_DATA_RACE:
+			return {
+				...state,
+				race: [...action.race],
+				detail: true
+			}
+		case TOGGLE_DATA_DISPLAY:
+			return {
+				...state,
+				detail: !state.detail
+			}
+		default:
+			return state
+	}
 }
 
 export { data }
